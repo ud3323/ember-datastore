37d904f78a041c33018e88ffb0ede15f8d28cedc
Edward Faulkner <ef@alum.mit.edu>
2011-08-11 22:51:45 -0400

    Make RecordAttributes cacheable
    RecordAttributes are not cacheable, despite having some code that
    looks like it was originally intended to make them so.  This has a
    noticeable performance impact, especially for Dates, which are
    relatively expensive to parse.

--- packages/ember-datastore/lib/attributes/record_attribute.js
+++ packages/ember-datastore/lib/attributes/record_attribute.js
@@ -312,9 +312,6 @@ Ember.RecordAttribute = Ember.Object.extend(
   /** @private - Make this look like a property so that `get()` will call it. */
   isProperty: YES,
 
-  /** @private - Make this look cacheable */
-  isCacheable: YES,
-
   /** @private - needed for KVO `property()` support */
   dependentKeys: [],
 
@@ -336,7 +333,7 @@ Ember.RecordAttribute = Ember.Object.extend(
     var attr = this;
     var ret  = Ember.computed(function(key, value) {
       return attr.call(this, key, value);
-    });
+    }).cacheable();
     ret.attr = attr;
     return ret ;
   }
